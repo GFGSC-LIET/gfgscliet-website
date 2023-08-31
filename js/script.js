0cd56771e6ee87791fb77ab1031b5f685f4cf221
p-sai-gowtham <puvvulasaigowtham@gmail.com>
2023-08-31 18:04:50 +0530

    added

--- js/script.js
+++ js/script.js
@@ -23,6 +23,47 @@ window.addEventListener('load', function () {
 
 });
 
+// header
+
+
+(function ($) {
+    "use strict";
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+    
+})(jQuery);
+
+
+
 
 
 
