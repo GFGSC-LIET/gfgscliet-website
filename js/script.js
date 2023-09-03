94743b439651e13cc609bead54afbbeb44a10e09
p-sai-gowtham <puvvulasaigowtham@gmail.com>
2023-09-03 17:52:42 +0530

    added

--- js/script.js
+++ js/script.js
@@ -1,259 +1,32 @@
 const checkBox = document.querySelector('.checkbox');
 const dr = document.querySelectorAll('.dr');
+const drw = document.querySelectorAll('.drw');
+const drp = document.querySelectorAll('.drp');
+const page = document.querySelector('.page');
+const preloader = document.querySelector('.preloader');
+
+page.style.display = 'none';
+
+window.addEventListener('load', function () {
+	preloader.style.display = 'none';
+	page.style.display = 'block';
+});
 
 
 checkBox.addEventListener('change', function () { 
   for (var i = 0; i < dr.length; i++) {
     if (checkBox.checked == true){
-      dr[i].classList.add("dark");
+		dr[i].classList.add("dark");
+		drw[i].classList.add("darkw");
+		drp[i].classList.add("darkp");
       } else {
-       dr[i].classList.remove("dark");
+		dr[i].classList.remove("dark");
+		drw[i].classList.remove("darkw");
+		drp[i].classList.remove("darkp");
     }
 
   }
 });
-
-
-
-"use strict";
-(function () {
-	// Global variables
-	var
-		userAgent = navigator.userAgent.toLowerCase(),
-		initialDate = new Date(),
-
-		$document = $(document),
-		$window = $(window),
-		$html = $("html"),
-		$body = $("body"),
-
-		isDesktop = $html.hasClass("desktop"),
-		isIE = userAgent.indexOf("msie") !== -1 ? parseInt(userAgent.split("msie")[1], 10) : userAgent.indexOf("trident") !== -1 ? 11 : userAgent.indexOf("edge") !== -1 ? 12 : false,
-		isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
-		windowReady = false,
-		isNoviBuilder = false,
-		livedemo = true,
-
-		plugins = {
-				preloader: $(".preloader"),
-			swiper:                  $( '.swiper-container' )
-		};
-
-	
-
-
-
-	// Initialize scripts that require a finished document
-	$(function () {
-		isNoviBuilder = window.xMode;
-
-		/**
-		 * @desc Toggle swiper videos on active slides
-		 * @param {object} swiper - swiper slider
-		 */
-		function toggleSwiperInnerVideos(swiper) {
-			var prevSlide = $(swiper.slides[swiper.previousIndex]),
-				nextSlide = $(swiper.slides[swiper.activeIndex]),
-				videos,
-				videoItems = prevSlide.find("video");
-
-			for (var i = 0; i < videoItems.length; i++) {
-				videoItems[i].pause();
-			}
-
-			videos = nextSlide.find("video");
-			if (videos.length) {
-				videos.get(0).play();
-			}
-		}
-
-		/**
-		 * @desc Toggle swiper animations on active slides
-		 * @param {object} swiper - swiper slider
-		 */
-		function toggleSwiperCaptionAnimation(swiper) {
-			var prevSlide = $(swiper.container).find("[data-caption-animate]"),
-				nextSlide = $(swiper.slides[swiper.activeIndex]).find("[data-caption-animate]"),
-				delay,
-				duration,
-				nextSlideItem,
-				prevSlideItem;
-
-			for (var i = 0; i < prevSlide.length; i++) {
-				prevSlideItem = $(prevSlide[i]);
-
-				prevSlideItem.removeClass("animated")
-					.removeClass(prevSlideItem.attr("data-caption-animate"))
-					.addClass("not-animated");
-			}
-
-
-			var tempFunction = function (nextSlideItem, duration) {
-				return function () {
-					nextSlideItem
-						.removeClass("not-animated")
-						.addClass(nextSlideItem.attr("data-caption-animate"))
-						.addClass("animated");
-					if (duration) {
-						nextSlideItem.css('animation-duration', duration + 'ms');
-					}
-				};
-			};
-
-			for (var i = 0; i < nextSlide.length; i++) {
-				nextSlideItem = $(nextSlide[i]);
-				delay = nextSlideItem.attr("data-caption-delay");
-				duration = nextSlideItem.attr('data-caption-duration');
-				if (!isNoviBuilder) {
-					if (delay) {
-						setTimeout(tempFunction(nextSlideItem, duration), parseInt(delay, 10));
-					} else {
-						tempFunction(nextSlideItem, duration);
-					}
-
-				} else {
-					nextSlideItem.removeClass("not-animated")
-				}
-			}
-		}
-
-		/**
-		 * @desc Initialize owl carousel plugin
-		 * @param {object} carousel - carousel jQuery object
-		 */
-		function initOwlCarousel(c) {
-			var aliaces = ["-", "-xs-", "-sm-", "-md-", "-lg-", "-xl-"],
-				values = [0, 480, 768, 992, 1200, 1600],
-				responsive = {};
-
-			for (var j = 0; j < values.length; j++) {
-				responsive[values[j]] = {};
-				for (var k = j; k >= -1; k--) {
-					if (!responsive[values[j]]["items"] && c.attr("data" + aliaces[k] + "items")) {
-						responsive[values[j]]["items"] = k < 0 ? 1 : parseInt(c.attr("data" + aliaces[k] + "items"), 10);
-					}
-					if (!responsive[values[j]]["stagePadding"] && responsive[values[j]]["stagePadding"] !== 0 && c.attr("data" + aliaces[k] + "stage-padding")) {
-						responsive[values[j]]["stagePadding"] = k < 0 ? 0 : parseInt(c.attr("data" + aliaces[k] + "stage-padding"), 10);
-					}
-					if (!responsive[values[j]]["margin"] && responsive[values[j]]["margin"] !== 0 && c.attr("data" + aliaces[k] + "margin")) {
-						responsive[values[j]]["margin"] = k < 0 ? 30 : parseInt(c.attr("data" + aliaces[k] + "margin"), 10);
-					}
-				}
-			}
-
-			// Enable custom pagination
-			if (c.attr('data-dots-custom')) {
-				c.on("initialized.owl.carousel", function (event) {
-					var carousel = $(event.currentTarget),
-						customPag = $(carousel.attr("data-dots-custom")),
-						active = 0;
-
-					if (carousel.attr('data-active')) {
-						active = parseInt(carousel.attr('data-active'), 10);
-					}
-
-					carousel.trigger('to.owl.carousel', [active, 300, true]);
-					customPag.find("[data-owl-item='" + active + "']").addClass("active");
-
-					customPag.find("[data-owl-item]").on('click', function (e) {
-						e.preventDefault();
-						carousel.trigger('to.owl.carousel', [parseInt(this.getAttribute("data-owl-item"), 10), 300, true]);
-					});
-
-					carousel.on("translate.owl.carousel", function (event) {
-						customPag.find(".active").removeClass("active");
-						customPag.find("[data-owl-item='" + event.item.index + "']").addClass("active")
-					});
-				});
-			}
-
-			c.on("initialized.owl.carousel", function () {
-				initLightGalleryItem(c.find('[data-lightgallery="item"]'), 'lightGallery-in-carousel');
-			});
-
-			c.owlCarousel({
-				autoplay: isNoviBuilder ? false : c.attr("data-autoplay") === "true",
-				loop: isNoviBuilder ? false : c.attr("data-loop") !== "false",
-				items: 1,
-				center: c.attr("data-center") === "true",
-				dotsContainer: c.attr("data-pagination-class") || false,
-				navContainer: c.attr("data-navigation-class") || false,
-				mouseDrag: isNoviBuilder ? false : c.attr("data-mouse-drag") !== "false",
-				nav: c.attr("data-nav") === "true",
-				dots: ( isNoviBuilder && c.attr("data-nav") !== "true" ) ? true : c.attr("data-dots") === "true",
-				dotsEach: c.attr("data-dots-each") ? parseInt(c.attr("data-dots-each"), 10) : false,
-				animateIn: c.attr('data-animation-in') ? c.attr('data-animation-in') : false,
-				animateOut: c.attr('data-animation-out') ? c.attr('data-animation-out') : false,
-				responsive: responsive,
-				navText: c.attr("data-nav-text") ? $.parseJSON( c.attr("data-nav-text") ) : [],
-				navClass: c.attr("data-nav-class") ? $.parseJSON( c.attr("data-nav-class") ) : ['owl-prev', 'owl-next']
-			});
-		}
-		// Swiper
-		if (plugins.swiper.length) {
-			for (var i = 0; i < plugins.swiper.length; i++) {
-				var s = $(plugins.swiper[i]);
-				var pag = s.find(".swiper-pagination"),
-					next = s.find(".swiper-button-next"),
-					prev = s.find(".swiper-button-prev"),
-					bar = s.find(".swiper-scrollbar"),
-					swiperSlide = s.find(".swiper-slide"),
-					autoplay = false;
-
-				for (var j = 0; j < swiperSlide.length; j++) {
-					var $this = $(swiperSlide[j]),
-						url;
-
-					if (url = $this.attr("data-slide-bg")) {
-						$this.css({
-							"background-image": "url(" + url + ")",
-							"background-size": "cover"
-						})
-					}
-				}
-
-				swiperSlide.end()
-					.find("[data-caption-animate]")
-					.addClass("not-animated")
-					.end();
-
-				s.swiper({
-					autoplay: !isNoviBuilder && $.isNumeric( s.attr('data-autoplay') ) ? s.attr('data-autoplay') : false,
-					direction: s.attr('data-direction') ? s.attr('data-direction') : "horizontal",
-					effect: s.attr('data-slide-effect') ? s.attr('data-slide-effect') : "slide",
-					speed: s.attr('data-slide-speed') ? s.attr('data-slide-speed') : 600,
-					keyboardControl: s.attr('data-keyboard') === "true",
-					mousewheelControl: s.attr('data-mousewheel') === "true",
-					mousewheelReleaseOnEdges: s.attr('data-mousewheel-release') === "true",
-					nextButton: next.length ? next.get(0) : null,
-					prevButton: prev.length ? prev.get(0) : null,
-					pagination: pag.length ? pag.get(0) : null,
-					paginationClickable: pag.length ? pag.attr("data-clickable") !== "false" : false,
-					paginationBulletRender: pag.length ? pag.attr("data-index-bullet") === "true" ? function (swiper, index, className) {
-						return '<span class="' + className + '">' + (index + 1) + '</span>';
-					} : null : null,
-					scrollbar: bar.length ? bar.get(0) : null,
-					scrollbarDraggable: bar.length ? bar.attr("data-draggable") !== "false" : true,
-					scrollbarHide: bar.length ? bar.attr("data-draggable") === "false" : false,
-					loop: isNoviBuilder ? false : s.attr('data-loop') !== "false",
-					simulateTouch: s.attr('data-simulate-touch') && !isNoviBuilder ? s.attr('data-simulate-touch') === "true" : false,
-					onTransitionStart: function (swiper) {
-						toggleSwiperInnerVideos(swiper);
-					},
-					onTransitionEnd: function (swiper) {
-						toggleSwiperCaptionAnimation(swiper);
-					},
-					onInit: function (swiper) {
-						toggleSwiperInnerVideos(swiper);
-						toggleSwiperCaptionAnimation(swiper);
-						initLightGalleryItem(s.find('[data-lightgallery="item"]'), 'lightGallery-in-carousel');
-					}
-				});
-			}
-		}
-
-	});
-}());
 
 
 // faq
@@ -287,32 +60,7 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
   });
 });
 
-// Initialize scripts that require a loaded page
-	$window.on('load', function () {
-		// Page loader & Page transition
-		if (plugins.preloader.length && !isNoviBuilder) {
-			pageTransition({
-				target: document.querySelector('.page'),
-				delay: 0,
-				duration: pageTransitionAnimationDuration,
-				classActive: 'animated',
-				conditions: function (event, link) {
-					return !/(\#|callto:|tel:|mailto:|:\/\/)/.test(link)
-							&& !event.currentTarget.hasAttribute('data-lightgallery')
-							&& event.currentTarget.getAttribute('href') !== 'javascript:void(0);';
-				},
-				onTransitionStart: function (options) {
-					setTimeout(function () {
-						plugins.preloader.removeClass('loaded');
-					}, options.duration * .75);
-				},
-				onReady: function () {
-					plugins.preloader.addClass('loaded');
-					windowReady = true;
-				}
-			});
-		}
-	});
+
 
 
 
