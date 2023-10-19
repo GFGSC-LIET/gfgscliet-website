8ad732fd9447112cd650909a7f02d597c50c8a7a
Reshma Tejeswani Patarapalli <reshmatejeswani@gmail.com>
2023-10-19 21:16:34 +0530

    gallery

---
+++ js/gallery.js
@@ -0,0 +1,210 @@
+
+
+// gallery js starts here
+
+
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.style.width = "800px";
+    modalImg.style.height = "600px";
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+const checkBox = document.querySelector('.checkbox');
+const dr = document.querySelectorAll('.dr');
+const drw = document.querySelectorAll('.drw');
+const drp = document.querySelectorAll('.drp');
+const page = document.querySelector('.page');
+const preloader = document.querySelector('.preloader');
+
+page.style.display = 'none';
+
+window.addEventListener('load', function () {
+	preloader.style.display = 'none';
+	page.style.display = 'block';
+});
+
+
+checkBox.addEventListener('change', function () { 
+  for (var i = 0; i < dr.length; i++) {
+    if (checkBox.checked == true){
+		dr[i].classList.add("dark");
+		drw[i].classList.add("darkw");
+		drp[i].classList.add("darkp");
+      } else {
+		dr[i].classList.remove("dark");
+		drw[i].classList.remove("darkw");
+		drp[i].classList.remove("darkp");
+    }
+
+  }
+});
+
+
+
+"use strict";
+(function () {
+	// Global variables
+	var userAgent = navigator.userAgent.toLowerCase(),
+		initialDate = new Date(),
+
+		$document = $(document),
+		$window = $(window),
+		$html = $("html"),
+		$body = $("body"),
+
+		isDesktop = $html.hasClass("desktop"),
+		isIE = userAgent.indexOf("msie") !== -1 ? parseInt(userAgent.split("msie")[1], 10) : userAgent.indexOf("trident") !== -1 ? 11 : userAgent.indexOf("edge") !== -1 ? 12 : false,
+		isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+		windowReady = false,
+		isNoviBuilder = false,
+		loaderTimeoutId,
+
+		plugins = {
+			bootstrapModalDialog: $('.modal'),
+			bootstrapTabs: $(".tabs-custom"),
+			rdNavbar: $(".rd-navbar"),
+			materialParallax: $(".parallax-container"),
+			rdMailForm: $(".rd-mailform"),
+			rdInputLabel: $(".form-label"),
+			regula: $("[data-constraints]"),
+			wow: $(".wow"),
+			owl: $(".owl-carousel"),
+			swiper: $(".swiper-slider"),
+			isotope: $(".isotope"),
+			radio: $("input[type='radio']"),
+			checkbox: $("input[type='checkbox']"),
+			customToggle: $("[data-custom-toggle]"),
+			counter: $(".counter"),
+			preloader: $(".preloader"),
+			captcha: $('.recaptcha'),
+			scroller: $(".scroll-wrap"),
+			lightGallery: $('[data-lightgallery="group"]'),
+			lightGalleryItem: $('[data-lightgallery="item"]'),
+			lightDynamicGalleryItem: $('[data-lightgallery="dynamic"]'),
+			copyrightYear: $(".copyright-year"),
+			maps: $(".google-map-container"),
+			particlesJs: $('#particles-js')
+		};
+
+
+	// Initialize scripts that require a finished document
+	$(function () {
+		isNoviBuilder = window.xMode;
+		// RD Navbar
+		if (plugins.rdNavbar.length) {
+			var aliaces, i, j, len, value, values, responsiveNavbar;
+
+			aliaces = ["-", "-sm-", "-md-", "-lg-", "-xl-", "-xxl-"];
+			values = [0, 576, 768, 992, 1200, 1600];
+			responsiveNavbar = {};
+
+			for (i = j = 0, len = values.length; j < len; i = ++j) {
+				value = values[i];
+				if (!responsiveNavbar[values[i]]) {
+					responsiveNavbar[values[i]] = {};
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'layout')) {
+					responsiveNavbar[values[i]].layout = plugins.rdNavbar.attr('data' + aliaces[i] + 'layout');
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout')) {
+					responsiveNavbar[values[i]]['deviceLayout'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout');
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on')) {
+					responsiveNavbar[values[i]]['focusOnHover'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on') === 'true';
+				}
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height')) {
+					responsiveNavbar[values[i]]['autoHeight'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height') === 'true';
+				}
+
+				if (isNoviBuilder) {
+					responsiveNavbar[values[i]]['stickUp'] = false;
+				} else if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up')) {
+					responsiveNavbar[values[i]]['stickUp'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up') === 'true';
+				}
+
+				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset')) {
+					responsiveNavbar[values[i]]['stickUpOffset'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset');
+				}
+			}
+			plugins.rdNavbar.RDNavbar({
+				anchorNav: !isNoviBuilder,
+				stickUpClone: (plugins.rdNavbar.attr("data-stick-up-clone") && !isNoviBuilder) ? plugins.rdNavbar.attr("data-stick-up-clone") === 'true' : false,
+				responsive: responsiveNavbar,
+				callbacks: {
+					onStuck: function () {
+						var navbarSearch = this.$element.find('.rd-search input');
+
+						if (navbarSearch) {
+							navbarSearch.val('').trigger('propertychange');
+						}
+					},
+					onDropdownOver: function () {
+						return !isNoviBuilder;
+					},
+					onUnstuck: function () {
+						if (this.$clone === null)
+							return;
+
+						var navbarSearch = this.$clone.find('.rd-search input');
+
+						if (navbarSearch) {
+							navbarSearch.val('').trigger('propertychange');
+							navbarSearch.trigger('blur');
+						}
+
+					}
+				}
+			});
+			if (plugins.rdNavbar.attr("data-body-class")) {
+				document.body.className += ' ' + plugins.rdNavbar.attr("data-body-class");
+			}
+		}
+	});
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
